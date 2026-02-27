cf1f6543c8e6acb7b7b6751d09749848e1ba6a58
Edward Lee <e45lee@uwaterloo.ca>
2026-02-27 15:22:42 -0500

    Add example, wordasm.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+from codetypes import *
+
+# A sequence of words that assembles to "Hello World\n"
+CODE = [
+    Word(0x6c6c6548),
+    Word(0x6f57206f),
+    Word(0x0a646c72)
+]
--- wordasm.py
+++ wordasm.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import sys
 from codetypes import *
+import argparse
+from importlib.machinery import SourceFileLoader
+import importlib.util
 
 def assembleWords(code: list[Code], out = sys.stdout.buffer):
     """ Assembles a List of Code objects to the target file. """
@@ -11,3 +14,15 @@ def assembleWords(code: list[Code], out = sys.stdout.buffer):
                     out.write(bytes((v >> i*8) & 0xff for i in (0, 1, 2, 3)))
                 else:
                     raise Exception("Word({0}) is out of range".format(v))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                    prog="wordasm",
+                    description="Assembles a sequence of Word objects")
+    parser.add_argument("filename")
+    args = parser.parse_args()
+    
+    spec = importlib.util.spec_from_file_location("code", args.filename)
+    code = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(code)
+    assembleWords(code.CODE) 
